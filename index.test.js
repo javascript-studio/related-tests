dacfe61fdd199db1453a6b5e1d8ac5c247dfdcff
Maximilian Antoni <mail@maxantoni.de>
2021-05-29 14:42:50 +0200

    Use fs.readdir to find related tests

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs/promises');
-const { assert, sinon } = require('@sinonjs/referee-sinon');
+const { assert, refute, sinon } = require('@sinonjs/referee-sinon');
 const withRelatedTests = require('.');
 
 describe('withRelatedTests', () => {
@@ -20,13 +20,19 @@ describe('withRelatedTests', () => {
   });
 
   it('returns file ending with .test.js', async () => {
+    sinon.replace(fs, 'readdir', sinon.fake.resolves(['some.test.js']));
+
     const script = await lintStaged(['some.test.js']);
 
     assert.equals(script, `${command} some.test.js`);
   });
 
-  it('returns matching tests if they exist', async () => {
-    sinon.replace(fs, 'access', sinon.fake.resolves());
+  it('returns matching .test.js files if they exist', async () => {
+    sinon.replace(
+      fs,
+      'readdir',
+      sinon.fake.resolves(['foo.js', 'foo.test.js', 'bar.js', 'bar.test.js'])
+    );
 
     const script = await lintStaged(['foo.js', 'bar.js']);
 
@@ -34,7 +40,7 @@ describe('withRelatedTests', () => {
   });
 
   it('does not return matching test they do not exist', async () => {
-    sinon.replace(fs, 'access', sinon.fake.rejects(new Error()));
+    sinon.replace(fs, 'readdir', sinon.fake.resolves(['foo.js', 'bar.js']));
 
     const script = await lintStaged(['foo.js', 'bar.js']);
 
@@ -42,7 +48,11 @@ describe('withRelatedTests', () => {
   });
 
   it('returns file and matching test only once', async () => {
-    sinon.replace(fs, 'access', sinon.fake.resolves());
+    sinon.replace(
+      fs,
+      'readdir',
+      sinon.fake.resolves(['foo.js', 'foo.test.js', 'bar.js', 'bar.test.js'])
+    );
 
     const script = await lintStaged([
       'foo.js',
@@ -52,5 +62,51 @@ describe('withRelatedTests', () => {
     ]);
 
     assert.equals(script, `${command} foo.test.js bar.test.js`);
+  });
+
+  it('does not call fs.readdir for .test.js files', async () => {
+    sinon.replace(fs, 'readdir', sinon.fake.rejects(new Error('Unexpected')));
+
+    await lintStaged(['a.test.js', 'foo/b.test.js']);
+
+    refute.called(fs.readdir);
+  });
+
+  it('only calls fs.readdir once per directory', async () => {
+    sinon.replace(fs, 'readdir', sinon.fake.resolves([]));
+
+    const script = await lintStaged([
+      'foo/a.js',
+      'foo/b.js',
+      'bar/c.js',
+      'bar/d.js',
+      'bar/e.js'
+    ]);
+
+    assert.calledTwice(fs.readdir);
+    assert.calledWith(fs.readdir, 'foo');
+    assert.calledWith(fs.readdir, 'bar');
+    assert.equals(script, 'true');
+  });
+
+  it('only adds matching files in same directory', async () => {
+    sinon.replace(
+      fs,
+      'readdir',
+      sinon.fake((dir) => {
+        switch (dir) {
+          case 'foo':
+            return Promise.resolve(['file.test.js']);
+          case 'bar':
+            return Promise.resolve(['other.test.js']);
+          default:
+            throw new Error(`Unexpected dir "${dir}"`);
+        }
+      })
+    );
+
+    const script = await lintStaged(['foo/file.js', 'bar/file.js']);
+
+    assert.equals(script, `${command} foo/file.test.js`);
   });
 });
