1007aeaeb258e5989c0655fcf3faebdb6d34598d
Maximilian Antoni <mail@maxantoni.de>
2021-05-29 14:42:50 +0200

    Use fs.readdir to find related tests

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path');
 const fs = require('fs/promises');
 
 module.exports = withRelatedTests;
@@ -13,18 +14,31 @@ function withRelatedTests(command) {
 
 async function findTests(files) {
   const tests = new Set();
+  const dirs_to_files = new Map();
+
   for (const file of files) {
     if (file.endsWith('.test.js')) {
       tests.add(file);
+      // Already matching, no need to read the directory
       continue;
     }
-    const test = file.replace(/\.js$/, '.test.js');
-    try {
-      await fs.access(test); // eslint-disable-line node/no-sync
-    } catch (e) {
-      continue;
+    // Map directories to files:
+    const dir = path.dirname(file);
+    if (!dirs_to_files.has(dir)) {
+      dirs_to_files.set(dir, []);
     }
-    tests.add(test);
+    dirs_to_files.get(dir).push(path.basename(file, '.js'));
+  }
+
+  for (const [dir, files_in_dir] of dirs_to_files.entries()) {
+    const dir_files = await fs.readdir(dir);
+    for (const dir_file of dir_files) {
+      for (const file_in_dir of files_in_dir) {
+        if (dir_file.startsWith(file_in_dir) && dir_file.endsWith('.test.js')) {
+          tests.add(path.join(dir, dir_file));
+        }
+      }
+    }
   }
   return Array.from(tests);
 }
